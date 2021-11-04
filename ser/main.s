5952fb27cf4a2009b30622b2e8c050673d119412
Nikolai Ryzhkov <namezys@gmail.com>
2021-11-05 00:42:20 +0100

    fix

--- ser/main.s
+++ ser/main.s
@@ -9,38 +9,44 @@ __zero_reg__ = 1
 .global	__vector_19
 	.type	__vector_19, @function
 __vector_19:
-	__gcc_isr 1
-	push r25
+/* prologue: naked */
+/* frame size = 0 */
+/* stack size = 0 */
+.L__stack_usage = 0
+/* #APP */
+ ;  113 "./src/main.cpp" 1
+	
+	push r16
+	in r16, __SREG__
+	push r16
 	push r30
 	push r31
-/* prologue: Signal */
-/* frame size = 0 */
-/* stack size = 3...7 */
-.L__stack_usage = 3 + __gcc_isr.n_pushed
-	lds r30,current_buffer
-	lds r31,current_buffer+1
-	ld r24,Z
-	cpse r24,__zero_reg__
-	rjmp .L2
-	lds r24,193
-	andi r24,lo8(-33)
-	sts 193,r24
-.L1:
-/* epilogue start */
+	lds r30, current_buffer
+	lds r31, current_buffer + 1
+	ld r16, Z+
+	cpi r16, 0
+	breq STOP_5
+	sts 198, r16
 	pop r31
 	pop r30
-	pop r25
-	__gcc_isr 2
+	pop r16
+	out __SREG__, r16
+	pop r16
 	reti
-.L2:
-	sts 198,r24
-	lds r24,current_buffer
-	lds r25,current_buffer+1
-	adiw r24,1
-	sts current_buffer+1,r25
-	sts current_buffer,r24
-	rjmp .L1
-	__gcc_isr 0,r24
+	STOP_5:
+	lds r16, 193
+	andi r16, 223
+	sts 193, r16
+	pop r31
+	pop r30
+	pop r16
+	out __SREG__, r16
+	pop r16
+	reti
+	
+ ;  0 "" 2
+/* #NOAPP */
+/* epilogue start */
 	.size	__vector_19, .-__vector_19
 	.section	.text._Z4initv,"ax",@progbits
 .global	_Z4initv
@@ -53,7 +59,7 @@ _Z4initv:
 	ldi r30,lo8(buffer)
 	ldi r31,hi8(buffer)
 	st Z,__zero_reg__
-	std Z+32,__zero_reg__
+	std Z+15,__zero_reg__
 /* epilogue start */
 	ret
 	.size	_Z4initv, .-_Z4initv
@@ -66,17 +72,17 @@ _Z4savePKc:
 /* stack size = 0 */
 .L__stack_usage = 0
 /* #APP */
- ;  127 "./src/main.cpp" 1
+ ;  201 "./src/main.cpp" 1
 	cli
  ;  0 "" 2
 /* #NOAPP */
 	movw r30,r24
 	ldi r26,lo8(buffer)
 	ldi r27,hi8(buffer)
-.L6:
+.L4:
 	ld r24,Z+
 	cpse r24,__zero_reg__
-	rjmp .L7
+	rjmp .L5
 	st X,__zero_reg__
 	ldi r24,lo8(buffer)
 	ldi r25,hi8(buffer)
@@ -86,15 +92,15 @@ _Z4savePKc:
 	ori r24,lo8(32)
 	sts 193,r24
 /* #APP */
- ;  135 "./src/main.cpp" 1
+ ;  209 "./src/main.cpp" 1
 	sei
  ;  0 "" 2
 /* #NOAPP */
 /* epilogue start */
 	ret
-.L7:
+.L5:
 	st X+,r24
-	rjmp .L6
+	rjmp .L4
 	.size	_Z4savePKc, .-_Z4savePKc
 	.section	.rodata.main.str1.1,"aMS",@progbits,1
 .LC0:
@@ -103,59 +109,17 @@ _Z4savePKc:
 .global	main
 	.type	main, @function
 main:
-	rcall .
-	rcall .
-	in r28,__SP_L__
-	in r29,__SP_H__
 /* prologue: function */
-/* frame size = 4 */
-/* stack size = 4 */
-.L__stack_usage = 4
-	sts 197,__zero_reg__
-	ldi r19,lo8(-49)
-	sts 196,r19
-	ldi r18,lo8(2)
-	sts 192,r18
-	ldi r25,lo8(24)
-	sts 193,r25
-	ldi r24,lo8(6)
-	sts 194,r24
-	sts 197,__zero_reg__
-	sts 196,r19
-	sts 192,r18
-	sts 193,r25
-	sts 194,r24
-	call _Z4initv
-.L11:
+/* frame size = 0 */
+/* stack size = 0 */
+.L__stack_usage = 0
 	ldi r24,lo8(.LC0)
 	ldi r25,hi8(.LC0)
 	call _Z4savePKc
-	std Y+1,__zero_reg__
-	std Y+2,__zero_reg__
-	std Y+3,__zero_reg__
-	std Y+4,__zero_reg__
-.L9:
-	ldd r24,Y+1
-	ldd r25,Y+2
-	ldd r26,Y+3
-	ldd r27,Y+4
-	cpi r24,85
-	sbci r25,35
-	sbci r26,8
-	cpc r27,__zero_reg__
-	brsh .L11
-	ldd r24,Y+1
-	ldd r25,Y+2
-	ldd r26,Y+3
-	ldd r27,Y+4
-	adiw r24,1
-	adc r26,__zero_reg__
-	adc r27,__zero_reg__
-	std Y+1,r24
-	std Y+2,r25
-	std Y+3,r26
-	std Y+4,r27
-	rjmp .L9
+	ldi r25,0
+	ldi r24,0
+/* epilogue start */
+	ret
 	.size	main, .-main
 .global	current_buffer
 	.section	.bss.current_buffer,"aw",@nobits
@@ -168,14 +132,14 @@ current_buffer:
 	.type	buffer_end, @object
 	.size	buffer_end, 2
 buffer_end:
-	.word	buffer+32
+	.word	buffer+15
 .global	buffer
 	.section	.bss.buffer,"aw",@nobits
-	.p2align	5
+	.p2align	4
 	.type	buffer, @object
-	.size	buffer, 33
+	.size	buffer, 16
 buffer:
-	.zero	33
+	.zero	16
 	.ident	"GCC: (Homebrew AVR GCC 11.1.0_1) 11.1.0"
 .global __do_copy_data
 .global __do_clear_bss
--- ser/src/main.cpp
+++ ser/src/main.cpp
@@ -100,20 +100,62 @@
 //    }
 //}
 
-const uint8_t size = 32;
-uint8_t buffer[size + 1] __attribute__(( aligned(32) ));
-const uint8_t* buffer_end = buffer + size;
-uint8_t* current_buffer;
+extern "C" {
+const uint8_t size = 256 - 1;
+uint8_t buffer[size + 1] __attribute__(( aligned(256)));
+const uint8_t *buffer_end = buffer + size;
+uint8_t *current_buffer;
 
+const uint8_t USART_TX_EMPTY_INT_MASK = (1 << UDRIE0);
+const uint8_t USART_TX_EMPTY_INT_INV_MASK = static_cast<uint8_t>(~USART_TX_EMPTY_INT_MASK);
 
-ISR(USART_UDRE_vect)
-{
-    if (*current_buffer == 0) {
-        UCSR0B = UCSR0B & ~(1 << UDRIE0);
-    } else {
-        UDR0 = *current_buffer;
-        current_buffer++;
-    }
+ISR(USART_UDRE_vect, ISR_NAKED) {
+    asm volatile(
+            "\n\t"
+            // save state
+            "push r16\n\t"
+            "in r16, __SREG__\n\t"
+            "push r16\n\t"
+            "push r30\n\t"
+            "push r31\n\t"
+            // load buffer pointer and value
+            "lds r30, current_buffer\n\t"
+            "lds r31, current_buffer + 1\n\t"
+            "ld r16, Z+\n\t"
+            // check value and jump if necessary
+            "cpi r16, 0\n\t"
+            "breq STOP_%=\n\t"
+            // send to tx register and save new current buffer
+            // because of alignment, hi8 of this buffer will not changed
+            "sts %[RXD], r16\n\t"
+            "sts current_buffer, r30\n\t"
+            // restore state
+            "pop r31\n\t"
+            "pop r30\n\t"
+            "pop r16\n\t"
+            "out __SREG__, r16\n\t"
+            "pop r16\n\t"
+            "reti\n\t"
+            // stop send using interrupt
+            "STOP_%=:\n\t"
+            "lds r16, %[USART_B]\n\t"
+            "andi r16, %[USART_B_INT_MASK]\n\t"
+            "sts %[USART_B], r16\n\t"
+            // restore state
+            "pop r31\n\t"
+            "pop r30\n\t"
+            "pop r16\n\t"
+            "out __SREG__, r16\n\t"
+            "pop r16\n\t"
+            "reti\n\t"
+            :
+            :[USART_B]"M"(&UCSR0B),
+             [USART_B_INT_MASK]"M"(0xFF ^ USART_TX_EMPTY_INT_MASK),
+             [RXD]"M"(&UDR0),
+             "m"(current_buffer)
+    );
+}
+
 }
 
 void init()
@@ -124,6 +166,40 @@ void init()
 
 void save(const char* str)
 {
+
+    //  a6:	1f 92       	push	r1
+    //  a8:	0f 92       	push	r0
+    //  aa:	0f b6       	in	r0, 0x3f	; 63
+    //  ac:	0f 92       	push	r0
+    //  ae:	11 24       	eor	r1, r1
+    //  b0:	8f 93       	push	r24
+    //  b2:	9f 93       	push	r25
+    //  b4:	ef 93       	push	r30
+    //  b6:	ff 93       	push	r31
+    //  b8:	e0 91 20 01 	lds	r30, 0x0120	; 0x800120 <current_buffer>
+    //  bc:	f0 91 21 01 	lds	r31, 0x0121	; 0x800121 <current_buffer+0x1>
+    //  c0:	80 81       	ld	r24, Z
+    //  c2:	81 11       	cpse	r24, r1
+    //  c4:	0e c0       	rjmp	.+28     	; 0xe2 <__vector_19+0x3c>
+    //  c6:	80 91 c1 00 	lds	r24, 0x00C1	; 0x8000c1 <__TEXT_REGION_LENGTH__+0x7e00c1>
+    //  ca:	8f 7d       	andi	r24, 0xDF	; 223
+    //  cc:	80 93 c1 00 	sts	0x00C1, r24	; 0x8000c1 <__TEXT_REGION_LENGTH__+0x7e00c1>
+    //  d0:	ff 91       	pop	r31
+    //  d2:	ef 91       	pop	r30
+    //  d4:	9f 91       	pop	r25
+    //  d6:	8f 91       	pop	r24
+    //  d8:	0f 90       	pop	r0
+    //  da:	0f be       	out	0x3f, r0	; 63
+    //  dc:	0f 90       	pop	r0
+    //  de:	1f 90       	pop	r1
+    //  e0:	18 95       	reti
+    //  e2:	80 93 c6 00 	sts	0x00C6, r24	; 0x8000c6 <__TEXT_REGION_LENGTH__+0x7e00c6>
+    //  e6:	80 91 20 01 	lds	r24, 0x0120	; 0x800120 <current_buffer>
+    //  ea:	90 91 21 01 	lds	r25, 0x0121	; 0x800121 <current_buffer+0x1>
+    //  ee:	01 96       	adiw	r24, 0x01	; 1
+    //  f0:	90 93 21 01 	sts	0x0121, r25	; 0x800121 <current_buffer+0x1>
+    //  f4:	80 93 20 01 	sts	0x0120, r24	; 0x800120 <current_buffer>
+    //  f8:	eb cf       	rjmp	.-42     	; 0xd0 <__vector_19+0x2a>
     cli();
     uint8_t* b = buffer;
     for(;*str != 0; ++str, ++b) {
@@ -136,7 +212,7 @@ void save(const char* str)
 }
 
 int main() {
-    usart::Usart<usart::AsyncInternalClock<9600, true>> u; u.init();
+    usart::Usart<usart::AsyncInternalClock<9600, true>> u;
     init();
     while(1) {
         save("sdfghjkl");
@@ -144,7 +220,7 @@ int main() {
         for(volatile uint32_t i = 0; i < F_CPU / 30;) {
             i = i + 1;
         }
-        //u.send('+');
+        u.send('+');
     }
 //    init();
 //    send();
@@ -160,5 +236,5 @@ int main() {
 //
     //LOG_D("DDD");
 
-    return 0x1122;
+    return 0;
 }
