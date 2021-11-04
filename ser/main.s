52041e481ea82bc77ac2d8483f8b4ee772ddfa8c
Nikolai Ryzhkov <namezys@gmail.com>
2021-11-04 11:54:50 +0100

    upda

--- ser/main.s
+++ ser/main.s
@@ -5,7 +5,35 @@ __SREG__ = 0x3f
 __tmp_reg__ = 0
 __zero_reg__ = 1
 	.text
-	.section	.text.unlikely._Z5setupv,"ax",@progbits
+	.section	.text.print_p2,"ax",@progbits
+.global	print_p2
+	.type	print_p2, @function
+print_p2:
+/* prologue: function */
+/* frame size = 0 */
+/* stack size = 0 */
+.L__stack_usage = 0
+/* #APP */
+ ;  84 "./src/main.cpp" 1
+	
+LOAD_5:
+	lpm __zero_reg__, Z+
+	tst __zero_reg__
+	breq FINISH_5
+CHECK_5:
+	lds __tmp_reg__, 192
+	sbrs __tmp_reg__, 5
+	rjmp CHECK_5
+	sts 198, __zero_reg__
+	rjmp LOAD_5
+FINISH_5:
+
+ ;  0 "" 2
+/* #NOAPP */
+/* epilogue start */
+	ret
+	.size	print_p2, .-print_p2
+	.section	.text._Z5setupv,"ax",@progbits
 .global	_Z5setupv
 	.type	_Z5setupv, @function
 _Z5setupv:
@@ -13,15 +41,51 @@ _Z5setupv:
 /* frame size = 0 */
 /* stack size = 0 */
 .L__stack_usage = 0
+	ldi r25,0
+	ldi r24,0
+/* epilogue start */
+	ret
 	.size	_Z5setupv, .-_Z5setupv
-	.section	.text.startup.main,"ax",@progbits
-.global	main
-	.type	main, @function
-main:
+	.section	.text._ZN6StreamIN5usart5UsartINS0_18AsyncInternalClockILm9600ELb1EEENS0_5FrameILh8ELh1ELNS0_10ParityModeE0EEELb1ELb1ENS0_10DummyStateEEEE8print_p2EPKc,"axG",@progbits,_ZN6StreamIN5usart5UsartINS0_18AsyncInternalClockILm9600ELb1EEENS0_5FrameILh8ELh1ELNS0_10ParityModeE0EEELb1ELb1ENS0_10DummyStateEEEE8print_p2EPKc,comdat
+	.weak	_ZN6StreamIN5usart5UsartINS0_18AsyncInternalClockILm9600ELb1EEENS0_5FrameILh8ELh1ELNS0_10ParityModeE0EEELb1ELb1ENS0_10DummyStateEEEE8print_p2EPKc
+	.type	_ZN6StreamIN5usart5UsartINS0_18AsyncInternalClockILm9600ELb1EEENS0_5FrameILh8ELh1ELNS0_10ParityModeE0EEELb1ELb1ENS0_10DummyStateEEEE8print_p2EPKc, @function
+_ZN6StreamIN5usart5UsartINS0_18AsyncInternalClockILm9600ELb1EEENS0_5FrameILh8ELh1ELNS0_10ParityModeE0EEELb1ELb1ENS0_10DummyStateEEEE8print_p2EPKc:
 /* prologue: function */
 /* frame size = 0 */
 /* stack size = 0 */
 .L__stack_usage = 0
+	movw r30,r22
+/* #APP */
+ ;  54 "./src/main.cpp" 1
+	
+LOAD_27:
+	lpm __zero_reg__, Z+
+	tst __zero_reg__
+	breq FINISH_27
+CHECK_27:
+	lds __tmp_reg__, 192
+	sbrs __tmp_reg__, 5
+	rjmp CHECK_27
+	sts 198, __zero_reg__
+	rjmp LOAD_27
+FINISH_27:
+
+ ;  0 "" 2
+/* #NOAPP */
+/* epilogue start */
+	ret
+	.size	_ZN6StreamIN5usart5UsartINS0_18AsyncInternalClockILm9600ELb1EEENS0_5FrameILh8ELh1ELNS0_10ParityModeE0EEELb1ELb1ENS0_10DummyStateEEEE8print_p2EPKc, .-_ZN6StreamIN5usart5UsartINS0_18AsyncInternalClockILm9600ELb1EEENS0_5FrameILh8ELh1ELNS0_10ParityModeE0EEELb1ELb1ENS0_10DummyStateEEEE8print_p2EPKc
+	.section	.text.startup.main,"ax",@progbits
+.global	main
+	.type	main, @function
+main:
+	push __tmp_reg__
+	in r28,__SP_L__
+	in r29,__SP_H__
+/* prologue: function */
+/* frame size = 1 */
+/* stack size = 1 */
+.L__stack_usage = 1
 	sts 197,__zero_reg__
 	ldi r24,lo8(-49)
 	sts 196,r24
@@ -31,39 +95,47 @@ main:
 	sts 193,r24
 	ldi r24,lo8(6)
 	sts 194,r24
-	ldi r18,lo8(45)
-.L7:
-	ldi r30,lo8(_ZZ4mainE3__c)
-	ldi r31,hi8(_ZZ4mainE3__c)
-.L5:
+	ldi r22,lo8(_ZZ4mainE3__c)
+	ldi r23,hi8(_ZZ4mainE3__c)
+	movw r24,r28
+	adiw r24,1
+	call _ZN6StreamIN5usart5UsartINS0_18AsyncInternalClockILm9600ELb1EEENS0_5FrameILh8ELh1ELNS0_10ParityModeE0EEELb1ELb1ENS0_10DummyStateEEEE8print_p2EPKc
+	ldi r22,lo8(_ZZ4mainE3__c)
+	ldi r23,hi8(_ZZ4mainE3__c)
+	movw r24,r28
+	adiw r24,1
+	call _ZN6StreamIN5usart5UsartINS0_18AsyncInternalClockILm9600ELb1EEENS0_5FrameILh8ELh1ELNS0_10ParityModeE0EEELb1ELb1ENS0_10DummyStateEEEE8print_p2EPKc
+	ldi r22,lo8(_ZZ4mainE3__c_0)
+	ldi r23,hi8(_ZZ4mainE3__c_0)
+	movw r24,r28
+	adiw r24,1
+	call _ZN6StreamIN5usart5UsartINS0_18AsyncInternalClockILm9600ELb1EEENS0_5FrameILh8ELh1ELNS0_10ParityModeE0EEELb1ELb1ENS0_10DummyStateEEEE8print_p2EPKc
+	ldi r30,lo8(_ZZ4mainE3__c_1)
+	ldi r31,hi8(_ZZ4mainE3__c_1)
 /* #APP */
- ;  61 "./src/main.cpp" 1
-	lpm r24, Z+
+ ;  104 "./src/main.cpp" 1
+	CALL print_p2
  ;  0 "" 2
 /* #NOAPP */
-	tst r24
-	breq .L8
-.L4:
-	lds r25,192
-	sbrs r25,5
-	rjmp .L4
-	sts 198,r24
-	rjmp .L5
-.L8:
-	ldi r24,lo8(100)
-	ldi r25,0
-.L6:
-	lds r19,192
-	sbrs r19,5
-	rjmp .L6
-	sts 198,r18
-	sbiw r24,1
-	brne .L6
-	rjmp .L7
+	ldi r24,lo8(34)
+	ldi r25,lo8(17)
+/* epilogue start */
+	pop __tmp_reg__
+	ret
 	.size	main, .-main
+	.section	.progmem.data._ZZ4mainE3__c_1,"a"
+	.type	_ZZ4mainE3__c_1, @object
+	.size	_ZZ4mainE3__c_1, 10
+_ZZ4mainE3__c_1:
+	.string	"asdfghjkl"
+	.section	.progmem.data._ZZ4mainE3__c_0,"a"
+	.type	_ZZ4mainE3__c_0, @object
+	.size	_ZZ4mainE3__c_0, 10
+_ZZ4mainE3__c_0:
+	.string	"SDFGHJKL:"
 	.section	.progmem.data._ZZ4mainE3__c,"a"
 	.type	_ZZ4mainE3__c, @object
-	.size	_ZZ4mainE3__c, 28
+	.size	_ZZ4mainE3__c, 6
 _ZZ4mainE3__c:
-	.string	"ABCDE0987654321\302\2471234567890"
+	.string	"ABCDE"
 	.ident	"GCC: (Homebrew AVR GCC 11.1.0_1) 11.1.0"
--- ser/src/main.cpp
+++ ser/src/main.cpp
@@ -14,7 +14,7 @@ public:
     PStr operator+(uint8_t a) {
         return PStr(ptr + a);
     }
-  
+    
     char load() const{
         return pgm_read_byte(ptr);
     }
@@ -33,28 +33,6 @@ public:
     }
     
     inline
-    void print_string_P(PStr str) {
-//        for(auto c = str.load(); c != 0;) {
-//            Output::send(c);
-//            str = str + 1;
-//            c = str.load();
-//        }
-        auto s = str.ptr;
-        do {
-            uint8_t value = 1;
-            __asm__(
-                "lpm %0, Z+"
-                : "=r"(value), "=z"(s)
-                : "z"(s)
-            );
-            if (value == 0) {
-                return;
-            }
-            //Output::send(value);
-        } while(true);
-    }
-    
-    inline
     void print_p(const char* str) {
         do {
             uint8_t value;
@@ -69,24 +47,116 @@ public:
             Output::send(value);
         } while(true);
     }
+    
+    inline
+    void print_p2(const char* str) {
+        uint8_t *tmp;
+        asm volatile("\n"
+                "LOAD_%=:\n\t"
+                "lpm __zero_reg__, Z+\n\t"
+                "tst __zero_reg__\n\t"
+                "breq FINISH_%=\n"
+                "CHECK_%=:\n\t"
+                "lds __tmp_reg__, %[USART_A]\n\t"
+                "sbrs __tmp_reg__, %[RXD_EM_FL]\n\t"
+                "rjmp CHECK_%=\n\t"
+                "sts %[RXD], __zero_reg__\n\t"
+                "rjmp LOAD_%=\n"
+                "FINISH_%=:\n"
+                :"=z"(tmp)
+                :"z"(str), [RXD_EM_FL]"I"(UDRE0), [USART_A]"M"(&UCSR0A), [RXD]"M"(&UDR0)
+                :
+                );
+    }
+    
+    inline
+    void ppp(const char* str) {
+        asm volatile(
+                "AAAA"
+                :
+                :"z"(str));
+    }
 };
 
-
-
-int setup() {
-
+extern "C"
+void print_p2() {
+    uint8_t *tmp;
+    asm volatile("\n"
+                 "LOAD_%=:\n\t"
+                 "lpm __zero_reg__, Z+\n\t"
+                 "tst __zero_reg__\n\t"
+                 "breq FINISH_%=\n"
+                 "CHECK_%=:\n\t"
+                 "lds __tmp_reg__, %[USART_A]\n\t"
+                 "sbrs __tmp_reg__, %[RXD_EM_FL]\n\t"
+                 "rjmp CHECK_%=\n\t"
+                 "sts %[RXD], __zero_reg__\n\t"
+                 "rjmp LOAD_%=\n"
+                 "FINISH_%=:\n"
+    :"=z"(tmp)
+    :[RXD_EM_FL]"I"(UDRE0), [USART_A]"M"(&UCSR0A), [RXD]"M"(&UDR0)
+    :
+    );
 }
 
-int main() {
-    const char* p = PSTR("ABCDE0987654321§1234567890");
+inline
+void ppp(const char* str) {
+    asm volatile("CALL print_p2"::"z"(str), "m"(print_p2));
+}
+
+int setup() {
+    return 0;
+}
+
+enum class Level {
+    Error,
+    Info,
+    Debug
+};
+
+template<Level _level, class LogStream>
+class Logger: public LogStream {
+private:
+    static const Level level = _level;
     
-    Stream<usart::Usart<usart::AsyncInternalClock<9600, true>>> u;
-    //u.send('A');
-    while(1) {
-        u.print_p(p);
-        for(int i = 0; i < 100; ++i) {
-            u.send('-');
-        }
+    static
+    bool is_error_enabled() {
+        return is_info_enabled() || level == Level::Error;
     }
+    
+    static
+    bool is_info_enabled() {
+        return is_debug_enabled() || level == Level::Error;
+    }
+    
+    static
+    bool is_debug_enabled() {
+        return level == Level::Debug;
+    }
+    
+public:
+    void log_info_p(const char* str) {
+        if(!is_info_enabled()) {
+            return;
+        }
+        LogStream::print_p("I: ");
+        LogStream::print_p(str);
+        LogStream::send('\n');
+    }
+};
+
+#define LOG_I(data) logger.log_info_p(PSTR(data))
+
+int main() {
+    Logger<Level::Debug, Stream<usart::Usart<usart::AsyncInternalClock<9600, true>>>> logger;
+    const char *p = PSTR("ABCDE");
+//    logger.print_p2(p);
+//    logger.print_p2(p);
+//    logger.print_p2(PSTR("SDFGHJKL:"));
+while(1) {
+    ppp(PSTR("asdfghjkl"));
+}
+    
+    //LOG_D("DDD");
     return 0x1122;
 }
